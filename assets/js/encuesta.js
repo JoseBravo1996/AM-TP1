cb7765796b1fdf1e98a742be80617c38587ca5e5
Jose Luis Bravo <bravo.jose.luis18@gmail.com>
2022-09-13 16:57:02 -0300

    add validation

--- assets/js/encuesta.js
+++ assets/js/encuesta.js
@@ -29,3 +29,61 @@ send.addEventListener("click", e => {
     "\n Email : " + email +
     "\n Comentario : " + comentario);
 });
+
+const nombre = document.querySelector("[name=nombre]");
+const apellido = document.querySelector("[name=apellido]");
+const fnacimiento = document.querySelector("[name=fnacimiento]");
+const sexo = document.querySelector("[name=sexo]");
+const gvaloracion = document.querySelector("[name=gvaloracion]");
+const email = document.querySelector("[name=email]");
+
+const setErrors = (message, field, isError = true) => {
+  if (isError) {
+    field.classList.add("invalid");
+    field.nextElementSibling.classList.add("error");
+    field.nextElementSibling.innerText = message;
+  } else {
+    field.classList.remove("invalid");
+    field.nextElementSibling.classList.remove("error");
+    field.nextElementSibling.innerText = "";
+  }
+}
+
+const validateEmptyField = (message, e) => {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  if (fieldValue.trim().length === 0) {
+    setErrors(message, field);
+  } else {
+    setErrors("", field, false);
+  }
+}
+
+const validateEmailFormat = e => {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+  if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+    setErrors("Ingrese un mail valido", field);
+  } else {
+    setErrors("", field, false);
+  }
+}
+
+const validateStringFormat = e => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    const regex = new RegExp(/^[a-zA-Z]+$/);
+    if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+      setErrors("Ingrese un formato valido", field);
+    } else {
+      setErrors("", field, false);
+    }
+  }
+
+nombre.addEventListener("blur", (e) => validateEmptyField("El Nombre es requerido.", e));
+apellido.addEventListener("blur", (e) => validateEmptyField("El Apellido es requerido.", e));
+fnacimiento.addEventListener("blur", (e) => validateEmptyField("La Fecha de Nacimiento es requerido.", e));
+sexo.addEventListener("blur", (e) => validateEmptyField("El Sexo es requerido.", e));
+email.addEventListener("blur", (e) => validateEmptyField("El Email es requerido.", e));
+email.addEventListener("input", validateEmailFormat);
