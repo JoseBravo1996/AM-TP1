74092c6c59bf7af0597645c1c6554343797ae73f
Jose Luis Bravo <bravo.jose.luis18@gmail.com>
2022-09-13 20:29:00 -0300

    change semantic style

--- assets/js/encuesta.js
+++ assets/js/encuesta.js
@@ -3,25 +3,25 @@ var cancel = document.getElementById("cancel");
 var reset = document.getElementById("reset");
 var send = document.getElementById("send");
 
-reset.addEventListener("click", e =>{
-    e.preventDefault();
-    form.reset();
+reset.addEventListener("click", e => {
+  e.preventDefault();
+  form.reset();
 });
 
 send.addEventListener("click", e => {
-    e.preventDefault();
-    var sexoID = document.getElementById("sexo"); 
-    var valoracionID = document.getElementById("gvaloracion"); 
+  e.preventDefault();
+  var sexoID = document.getElementById("sexo");
+  var valoracionID = document.getElementById("gvaloracion");
 
-    var nombre = document.getElementById('nombre').value;
-    var apellido = document.getElementById('apellido').value;
-    var fnacimiento = document.getElementById('fnacimiento').value;
-    var sexo = sexoID.options[sexoID.selectedIndex].text;
-    var gvaloracion = valoracionID.options[valoracionID.selectedIndex].text;
-    var email = document.getElementById('email').value;
-    var comentario = document.getElementById('comentario').value;
+  var nombre = document.getElementById('nombre').value;
+  var apellido = document.getElementById('apellido').value;
+  var fnacimiento = document.getElementById('fnacimiento').value;
+  var sexo = sexoID.options[sexoID.selectedIndex].text;
+  var gvaloracion = valoracionID.options[valoracionID.selectedIndex].text;
+  var email = document.getElementById('email').value;
+  var comentario = document.getElementById('comentario').value;
 
-    alert("Nombre : " + nombre + 
+  alert("Nombre : " + nombre +
     "\n Apellido : " + apellido +
     "\n F. Nacimiento : " + fnacimiento +
     "\n Sexo : " + sexo +
@@ -71,19 +71,34 @@ const validateEmailFormat = e => {
 }
 
 const validateStringFormat = e => {
-    const field = e.target;
-    const fieldValue = e.target.value;
-    const regex = new RegExp(/^[a-zA-Z]+$/);
-    if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
-      setErrors("Ingrese un formato valido", field);
-    } else {
-      setErrors("", field, false);
-    }
+  const field = e.target;
+  const fieldValue = e.target.value;
+  const regex = new RegExp(/^[a-zA-Z]+$/);
+  if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+    setErrors("Ingrese un formato valido", field);
+  } else {
+    setErrors("", field, false);
   }
+}
+
+const validateDateFormat = e => {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  const regex = new RegExp(/^[a-zA-Z]+$/);
+  if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+    setErrors("Ingrese un formato valido", field);
+  } else {
+    setErrors("", field, false);
+  }
+}
 
 nombre.addEventListener("blur", (e) => validateEmptyField("El Nombre es requerido.", e));
 apellido.addEventListener("blur", (e) => validateEmptyField("El Apellido es requerido.", e));
 fnacimiento.addEventListener("blur", (e) => validateEmptyField("La Fecha de Nacimiento es requerido.", e));
+
+nombre.addEventListener("input", validateStringFormat);
+apellido.addEventListener("input", validateStringFormat);
+
 sexo.addEventListener("blur", (e) => validateEmptyField("El Sexo es requerido.", e));
 email.addEventListener("blur", (e) => validateEmptyField("El Email es requerido.", e));
 email.addEventListener("input", validateEmailFormat);
