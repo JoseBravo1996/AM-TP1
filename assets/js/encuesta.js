0010fa6561e0aec90e9d07d310d04e7829a80fa3
Pablo Damian Escobar <pdescobar@prismamp.com>
2022-09-15 19:00:09 -0300

    arreglando selectores y agregando validaciones

--- assets/js/encuesta.js
+++ assets/js/encuesta.js
@@ -1,27 +1,34 @@
 var form = document.getElementById('form');
-var cancel = document.getElementById("cancel");
-var reset = document.getElementById("reset");
+var cancel = document.getElementById("cancelButtom");
+var reset = document.getElementById("resetButtom");
 var send = document.getElementById("send");
 
 reset.addEventListener("click", e => {
-  e.preventDefault();
-  form.reset();
+    e.preventDefault();
+    form.reset();
 });
 
-send.addEventListener("click", e => {
+cancel.addEventListener("click", e => {
   e.preventDefault();
-  var sexoID = document.getElementById("sexo");
-  var valoracionID = document.getElementById("gvaloracion");
+  userConfirm = confirm("Deseas volver al menu anterior?")
+  console.log(userConfirm);
+  if(userConfirm)  history.back();
+})
 
-  var nombre = document.getElementById('nombre').value;
-  var apellido = document.getElementById('apellido').value;
-  var fnacimiento = document.getElementById('fnacimiento').value;
-  var sexo = sexoID.options[sexoID.selectedIndex].text;
-  var gvaloracion = valoracionID.options[valoracionID.selectedIndex].text;
-  var email = document.getElementById('email').value;
-  var comentario = document.getElementById('comentario').value;
+send.addEventListener("click", e => {
+    e.preventDefault();
+    var sexoID = document.getElementById("sexo"); 
+    var valoracionID = document.getElementById("gvaloracion"); 
 
-  alert("Nombre : " + nombre +
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var fnacimiento = document.getElementById('fnacimiento').value;
+    var sexo = sexoID.options[sexoID.selectedIndex].text;
+    var gvaloracion = valoracionID.options[valoracionID.selectedIndex].text;
+    var email = document.getElementById('email').value;
+    var comentario = document.getElementById('comentario').value;
+
+    alert("Nombre : " + nombre + 
     "\n Apellido : " + apellido +
     "\n F. Nacimiento : " + fnacimiento +
     "\n Sexo : " + sexo +
@@ -71,34 +78,21 @@ const validateEmailFormat = e => {
 }
 
 const validateStringFormat = e => {
-  const field = e.target;
-  const fieldValue = e.target.value;
-  const regex = new RegExp(/^[a-zA-Z]+$/);
-  if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
-    setErrors("Ingrese un formato valido", field);
-  } else {
-    setErrors("", field, false);
+    const field = e.target;
+    const fieldValue = e.target.value;
+    const regex = new RegExp(/^[a-zA-Z]+$/);
+    if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+      setErrors("Ingrese un formato valido", field);
+    } else {
+      setErrors("", field, false);
+    }
   }
-}
-
-const validateDateFormat = e => {
-  const field = e.target;
-  const fieldValue = e.target.value;
-  const regex = new RegExp(/^[a-zA-Z]+$/);
-  if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
-    setErrors("Ingrese un formato valido", field);
-  } else {
-    setErrors("", field, false);
-  }
-}
 
 nombre.addEventListener("blur", (e) => validateEmptyField("El Nombre es requerido.", e));
 apellido.addEventListener("blur", (e) => validateEmptyField("El Apellido es requerido.", e));
 fnacimiento.addEventListener("blur", (e) => validateEmptyField("La Fecha de Nacimiento es requerido.", e));
-
-nombre.addEventListener("input", validateStringFormat);
-apellido.addEventListener("input", validateStringFormat);
-
 sexo.addEventListener("blur", (e) => validateEmptyField("El Sexo es requerido.", e));
 email.addEventListener("blur", (e) => validateEmptyField("El Email es requerido.", e));
+nombre.addEventListener("input", validateStringFormat);
+apellido.addEventListener("input", validateStringFormat);
 email.addEventListener("input", validateEmailFormat);
